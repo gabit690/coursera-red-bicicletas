740701490dee590fc831086bdd58a390265e9f90
gabit <gabit690@gmail.com>
2020-10-06 10:38:48 -0300

    Error en la configuracion de la statrategia

--- config/passport.js
+++ config/passport.js
@@ -5,8 +5,8 @@ const Usuario = require('../models/usuario');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.HOST + "/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
